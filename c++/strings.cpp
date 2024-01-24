6081d386ac25637ce3e00e11e4202ff0e19f1b6c
Lorak80 <144807930+Lorak80@users.noreply.github.com>
2024-01-24 23:28:00 +0100

    first few c++ programs

---
+++ c++/strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string wow_there_is_a_type_for_string = "oa9ewoaaaa";
+    string something;
+    string other = wow_there_is_a_type_for_string + " ciao a tutti!!!";
+    cout << other << "\n";
+
+    cout << wow_there_is_a_type_for_string.length() << endl;
+    something += other[0];
+    something += other[2];
+    cout << something << endl;
+
+    string input;
+    getline(cin, input);
+    cout << input << endl;
+
+    return 0;
+}
